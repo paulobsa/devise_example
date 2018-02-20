0849903d439a740d652e118f32cdd25fc11a167f
Paulo R. Araújo <pauloricardo722@gmail.com>
2018-02-19 23:25:53 -0300

    Remove the registerable option for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   enum role: [:user, :admin]
-  after_initialize :set_default_role, :if => :new_record?
+  #after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
   	self.role ||= :user
   end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
