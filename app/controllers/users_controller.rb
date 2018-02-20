15e52c79fe4be5672b8b8623daa31c4ff29fde2a
Paulo R. Araújo <pauloricardo722@gmail.com>
2018-02-20 15:37:31 -0300

    adding option to user change password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_admin
+  before_action :authorize_admin, except: [:change_password]
 
   # GET /users
   # GET /users.json
@@ -70,14 +70,10 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      unless current_user
-        @user = User.find(params[:id])
-      else
-        @user = current_user
-      end
+      @user = User.find(params[:id])
     end
 
-        # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation, :role)
     end
